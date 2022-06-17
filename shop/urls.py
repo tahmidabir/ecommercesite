cf30ab06f1d994666ce9e03ee25a4425315e01a7
tahmidabir <md.tahmid.chowdhury.abir@g.bracu.ac.bd>
2022-06-18 03:21:55 +0600

    added Migrate,Modals,admin , products

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,11 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('', views.index, name="ShopHome"),
+    path('about/', views.about, name="AboutUS"),
+    path('contact/', views.contact, name="ContactUS"),
+    path('tracker/', views.tracker, name="TrackingStatus"),
+    path('search/', views.search, name="Search"),
+    path('productview/', views.prodView, name="ProductView"),
+    path('checkout/', views.checkout, name="Checkout"),
 
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+
 # Create your views here.
 
+
+# path('about/', views.about, name="AboutUS"),
+# path('contact/', views.contact, name="ContactUS"),
+# path('tracker/', views.tracker, name="TrackingStatus"),
+# path('search/', views.search, name="Search"),
+# path('productview/', views.prodView, name="Product"),
+# path('checkout/', views.checkout, name="Checkout"),
+
+
 def index(request):
-    return render(request,'shop/index.html')
+    return render(request, 'shop/index.html')
+
+
+def about(request):
+    return HttpResponse("about")
+
+
+def contact(request):
+    return HttpResponse("contact")
+
+
+def tracker(request):
+    return HttpResponse("tracker")
+
+
+def search(request):
+    return HttpResponse("search")
+
+
+def prodView(request):
+    return HttpResponse("product view")
+
+
+def checkout(request):
+    return HttpResponse("checkout")
