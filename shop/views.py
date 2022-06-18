d6c393543d83502419958dee76336d81a41c2251
tahmidabir <md.tahmid.chowdhury.abir@g.bracu.ac.bd>
2022-06-19 01:34:44 +0600

    template interitance, parse products from database and showed in site

--- shop/views.py
+++ shop/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+from math import ceil
 
 
 # Create your views here.
 
 
-# path('about/', views.about, name="AboutUS"),
-# path('contact/', views.contact, name="ContactUS"),
-# path('tracker/', views.tracker, name="TrackingStatus"),
-# path('search/', views.search, name="Search"),
-# path('productview/', views.prodView, name="Product"),
-# path('checkout/', views.checkout, name="Checkout"),
-
-
 def index(request):
-    return render(request, 'shop/index.html')
+    products = Product.objects.all()
+    print(products)
+
+    n = len(products)
+    nslides = n // 4 + ceil((n / 4) - (n // 4))
+
+    params = {'no_of_slides':nslides, 'range':range(1,nslides), 'product':products}
+    return render(request, 'shop/index.html', params)
 
 
 def about(request):
